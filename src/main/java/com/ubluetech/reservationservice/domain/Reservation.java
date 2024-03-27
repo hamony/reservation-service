c42a36cd37eefa2b08076e0cccf146809855fee2
Khoa Nguyen <hamony@users.noreply.github.com>
2024-03-27 16:29:09 +0700

    BugFix - 5 crash container when deploying to k8s - https://github.com/hamony/reservation-service/issues/5 (#6)
    * Fixed Crash container when deploying to K8s https://github.com/hamony/reservation-service/issues/5
    
    * Fixed typo reservation service
    
    * Fixed Crash container when deploying to K8s https://github.com/hamony/reservation-service/issues/5
    
    Persistent object - Fixed Crash container when deploying to K8s
    
    ---------
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>

--- src/main/java/com/ubluetech/reservationservice/domain/Reservation.java
+++ src/main/java/com/ubluetech/reservationservice/domain/Reservation.java
@@ -1,6 +1,7 @@
 package com.ubluetech.reservationservice.domain;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -19,7 +20,12 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Guest guest;
-    private Room room;
+
+    @OneToMany(mappedBy = "reservation",
+               cascade = CascadeType.ALL,
+               orphanRemoval = true)   
+    private Set<Room> rooms;
+
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
 }
