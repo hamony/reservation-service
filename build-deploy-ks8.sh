916fb144efaa2bdee3d65d3faff3a928fdf108b2
Khoa Nguyen <hamony@users.noreply.github.com>
2024-04-09 18:25:12 +0700

    Use Kustomize to build app (#16)
    * BugFix - 5 crash container when deploying to k8s - https://github.com/hamony/reservation-service/issues/5 (#6)
    
    * Fixed Crash container when deploying to K8s https://github.com/hamony/reservation-service/issues/5
    
    * Fixed typo reservation service
    
    * Fixed Crash container when deploying to K8s https://github.com/hamony/reservation-service/issues/5
    
    Persistent object - Fixed Crash container when deploying to K8s
    
    ---------
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>
    
    * Apply deploy and build image on local by Google build docker (#8)
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>
    
    * Feature/using skaffold facilitates continuous development (#13)
    
    * Apply deploy and build image on local by Google build docker
    
    * Easy using skaffold on local development
    
    ---------
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>
    
    * Event Brainstoming and Kafka deploy (#15)
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>
    
    * Apply deploy and build image on local by Google build docker (#8)
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>
    
    * Event Brainstoming and Kafka deploy (#15)
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>
    
    ---------
    
    Co-authored-by: Khoa Nguyen <khoa.zamioza@gmail.com>

--- build-deploy-ks8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# eval $(minikube docker-env)
-# Jib google docker build with minikube
-./mvnw com.google.cloud.tools:jib-maven-plugin:dockerBuild -Dimage=reservation-service:v0.1.0-alpha
-
-# Build deployment and service
-kubectl apply -f ./k8s
